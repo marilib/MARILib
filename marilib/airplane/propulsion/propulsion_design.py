43a9f519bcfb13af0d1b3d65e4910da387c5c145
Thales Delmiro <thales.delmiro@irt-saintexupery.com>
2019-09-09 10:39:42 +0200

    Remove coupling variables in "TF" architecture

--- marilib/airplane/propulsion/propulsion_design.py
+++ marilib/airplane/propulsion/propulsion_design.py
@@ -229,13 +229,11 @@ def eval_battery_mass(aircraft):
     """
 
     if (aircraft.propulsion.architecture=="TF"):
-       aircraft.propulsion.battery_energy_density = 0.
-       aircraft.center_of_gravity.battery = 0.
-       aircraft.weights.battery_in_owe = 0.
+        aircraft.center_of_gravity.battery = 0.
     elif (aircraft.propulsion.architecture=="PTE1"):
-       eval_pte1_battery_mass(aircraft)
+        eval_pte1_battery_mass(aircraft)
     elif (aircraft.propulsion.architecture=="EF1"):
-       eval_ef1_battery_mass(aircraft)
+        eval_ef1_battery_mass(aircraft)
 
     return
 
